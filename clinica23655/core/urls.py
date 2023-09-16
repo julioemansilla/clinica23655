3f8a84a009d3e0cde4e64a8517188f8cc284f1bb
Nahuel Roldan <nahuel.roldan@hotmail.com.ar>
2023-09-16 17:03:23 -0300

    Union de proyecto

--- clinica23655/core/urls.py
+++ clinica23655/core/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('home/', views.index, name='index'),
     path('especialidades_listado/', views.especialidades_listado, name='especialidades_listado'),
-    path('turnos_dia/', views.turnos_dia, name='turnos_dia')
+    path('turnos_dia/', views.turnos_dia, name='turnos_dia') ,
+    path('medicos/', views.medicos, name='medicos')
 ]
--- clinica23655/core/views.py
+++ clinica23655/core/views.py
@@ -2,21 +2,22 @@ from django.http import HttpResponse
 import datetime
 from django.shortcuts import render
 
-"""def index(request):
-    return HttpResponse('index.html')"""
+
 
 def index(request):
-    return render ( request, 'index.html' )
+    return render ( request, 'core/index.html' )
 
 def especialidades_listado(request):
-    return HttpResponse("""
-<ul>
-    <li>Clinica General Adultos</li>
-    <li>Pediatria</li>
-    <li>Cardiologia</li>                    
-</ul>
-"""
-    )
+    return render (request, 'core/especialidades.html')
+
 def turnos_dia(request):
     now=datetime.datetime.now()
-    return HttpResponse(f'<h1> Turnos del dia: {now}</h1>')
+    return HttpResponse(f'''
+                        {{% block main %}}
+                        <h1> Turnos del dia: {now}</h1>
+                        {{% endblock %}}''')
+
+
+
+def medicos(request):
+    return render (request, 'core/medicos.html')
