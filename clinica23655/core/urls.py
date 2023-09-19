c23a6ec26aec0e6297bba519c7fc024f95fb0478
Jonatan Cabrera <cabrerajonatan270699@gmail.com>
2023-09-18 23:04:18 -0300

    Add Turno, se ordenaron css y html de turno.

--- clinica23655/core/urls.py
+++ clinica23655/core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from . import views
+from core.views import * 
+from turno.views import nuevo_turno
 
 urlpatterns = [
-    path('home/', views.index, name='index'),
-    path('home/<str:nombre_usuario>/', views.index2, name='nombre_usuario'),
-    path('especialidades_listado/', views.especialidades_listado, name='especialidades_listado'),
-    path('turnos_dia/', views.turnos_dia, name='turnos_dia') ,
-    path('medicos/', views.medicos, name='medicos'),
-    path('datos_personales_medico/<int:matricula>', views.datos_personales_medico, name="datos_personales_medico")
+    path('home/', index, name='index'),
+    path('home/<str:nombre_usuario>/', index2, name='nombre_usuario'),
+    path('especialidades_listado/', especialidades_listado, name='especialidades_listado'),
+    path('medicos/', medicos, name='medicos'),
+    path('datos_personales_medico/<int:matricula>', datos_personales_medico, name="datos_personales_medico"),
+    path('nuevo_turno/',nuevo_turno, name="nuevo_turno"),
 ]
