a57e0f472cb70e14a12301717c6f701d76e4f48d
Nahuel Roldan <nahuel.roldan@hotmail.com.ar>
2023-09-30 23:33:01 -0300

    Se adapta el formulario para la carga de Medicos

--- clinica23655/core/forms.py
+++ clinica23655/core/forms.py
@@ -3,6 +3,7 @@ from django import forms
 class ContactoForms(forms.Form):
     nombre = forms.CharField(label='Nombre')
     apellido = forms.CharField(label='Apellido')
-    dni = forms.CharField (label='Dni')
+    rubro = forms.CharField (label='Rubro')
     matricula = forms.CharField (label='Matricula')
+    dni = forms.CharField(label='Dni')
     email = forms.EmailField(label='Email')
