3ab8a09cfc85e1db185c7b2100b380c9c8d1f7eb
julioemansilla <julio.mansilla@bue.edu.ar>
2023-10-04 17:13:49 -0300

    Migracion y Creacion de tablas Inicial

--- clinica23655/clinica23655/settings.py
+++ clinica23655/clinica23655/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'clinica23655.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       "ENGINE": "django.db.backends.postgresql",
+        "NAME": "cac_2023_23655",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
