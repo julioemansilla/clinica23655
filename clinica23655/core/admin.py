80c3ec41b7263e979796098d017feabf17fed99f
Nahuel Roldan <nahuel.roldan@hotmail.com.ar>
2023-11-07 21:19:39 -0300

    Se personaliza el django admin

--- clinica23655/core/admin.py
+++ clinica23655/core/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-
 from core.models import *
 
-admin.site.register(Doctor)
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'matricula')
+    list_editable = ('nombre' , 'apellido')
+    list_display_links = ['matricula']
+    search_fields = ['apellido', 'nombre']
+
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'dni')
+    list_editable = ('nombre' , 'apellido')
+    list_display_links = ['dni']
+    search_fields = ['apellido', 'nombre','dni']
+
+class TurnoAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'Paciente', 'hora','fecha')
+    
+    
+
+admin.site.register(Doctor, DoctorAdmin)
 admin.site.register(Especialidad)
-admin.site.register(Paciente)
-admin.site.register(Turno)
+admin.site.register(Paciente, PacienteAdmin)
+admin.site.register(Turno, TurnoAdmin)
